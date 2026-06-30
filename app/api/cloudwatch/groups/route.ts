001d8f8f8d8161b5c53dce5c9420be0676945aa4
Thijs Verkade <thijs-verkade@hotmail.nl>
2026-06-30 10:54:32 +0200

    feat: cloudwatch domains + environments routes; env-aware tail

--- app/api/cloudwatch/groups/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import { getLogGroups } from '@/lib/sources/cloudwatch'
-
-export const dynamic = 'force-dynamic'
-
-export async function GET() {
-  return NextResponse.json(await getLogGroups())
-}
